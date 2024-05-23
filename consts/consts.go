cb177259040d26b5ae0fbcfc67ddadca344dd4ea
Solanachief <kennethkenddys@gmail.com>
2024-05-23 02:31:59 +0100

    changes

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "SolChain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SolHyperChain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SCHAN"
 )
 
 var ID ids.ID
